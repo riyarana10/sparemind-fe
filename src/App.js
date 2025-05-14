1f235c5cdd8011f8a39046eceb3f30409a0fcbc2
RiyaRanaShorthillsAI <riya.rana@shorthills.ai>
2025-05-14 14:26:37 +0530

    Resolved login page issue when user is not authenticated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import CategoryPage from "./pages/CategoryPage/CategoryPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
@@ -10,23 +10,30 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
 
-
 export default function App() {
+  const token = localStorage.getItem("access_token");
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Navbar />}>
-          <Route index element={<HomePage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="original/:code" element={<ProductDetails />} />
-          <Route path="replacement/:code" element={<ProductDetails />} />
-          <Route path="category/:name" element={<CategoryPage />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="all-categories" element={<AllCategoryPage />} />
-          <Route path="/product/:code" element={<ProductDetails />} />
-        </Route>
+        {token ? (
+          <Route path="/" element={<Navbar />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="original/:code" element={<ProductDetails />} />
+            <Route path="replacement/:code" element={<ProductDetails />} />
+            <Route path="category/:name" element={<CategoryPage />} />
+            <Route path="profile" element={<ProfilePage />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="all-categories" element={<AllCategoryPage />} />
+            <Route path="/product/:code" element={<ProductDetails />} />
+            <Route path="/login" element={<Navigate to="/" />} />
+          </Route>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
